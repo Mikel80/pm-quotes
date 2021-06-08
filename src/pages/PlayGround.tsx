ad5895ebc9ce31eebf4b39ddcda64fe5a8e7512d
Jose Miguel Garcia <josemiguelgarcia@Joses-MacBook-Pro.local>
2021-06-08 17:40:22 -0500

    Adding vendor hex

--- src/pages/PlayGround.tsx
+++ src/pages/PlayGround.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 
 const PlayGround = () => {
-  const getUsbDevices = async () => {
-    try {
-      const devices = await navigator.usb.getDevices();
-      devices.forEach((device) => {
-        console.log(device.vendorId);
-        console.log(device.productName);
-        console.log(device.manufacturerName);
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  //   const getUsbDevices = async () => {
+  //     try {
+  //       const devices = await navigator.usb.getDevices();
+  //       devices.forEach((device) => {
+  //         console.log(device.vendorId);
+  //         console.log(device.productName);
+  //         console.log(device.manufacturerName);
+  //       });
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
 
   const getG2Device = async () => {
     try {
       const device = navigator.usb.requestDevice({
-        filters: [{ vendorId: 0x1624 }],
+        filters: [{ vendorId: 0x0951 }],
       });
       console.log(device);
     } catch (err) {
@@ -28,7 +28,6 @@ const PlayGround = () => {
   return (
     <div>
       <button onClick={getG2Device}>Request G2</button>
-      <button onClick={getUsbDevices}>Request all Devices</button>
     </div>
   );
 };
